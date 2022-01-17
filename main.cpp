b7c7f40ed81b6773fd2e1e95ef20aeeccfc9a6fb
Dorian Gillioz <gilliozdorian@gmail.com>
2022-01-17 15:48:08 +0100

    saisies pour paramétrer le jeu et ajout de constantes

--- main.cpp
+++ main.cpp
@@ -14,24 +14,40 @@ Compilateur : Mingw-w64 g++ 11.2.0
 #include <chrono>
 #include <thread>
 
+#include "annexes.h"
 #include "Plateau.h"
 
 using namespace std;
 
 int main() {
-   std::cout << "Hello, World!" << std::endl;
+   cout << "Ce programme permet de simuler un combat de robots dans un plateau avec le "
+        << "nombre de robots et les dimensions du plateau parametrables." << endl;
+
+   const unsigned SLEEP = 200;
+   const unsigned MIN_LARGEUR    = 10,
+                  MAX_LARGEUR    = 1000,
+                  MIN_HAUTEUR    = 10,
+                  MAX_HAUTEUR    = 1000,
+                  MIN_NBR_OBJETS = 1,
+                  MAX_NBR_OBJETS = 9;
 
    // TODO : utiliser un using pour le type des coordonnées ?
 
-   Plateau plateau (20,10);
+   unsigned largeur = saisir<unsigned>("largeur ", "erreur de saisie", MIN_LARGEUR, MAX_LARGEUR);
+   unsigned hauteur = saisir<unsigned>("hauteur ", "erreur de saisie", MIN_HAUTEUR, MAX_HAUTEUR);
+   unsigned nbr_objets = saisir<unsigned>("nbre object ", "erreur de saisie", MIN_NBR_OBJETS, MAX_NBR_OBJETS);
 
-   plateau.ajouterRobots(10);
+   Plateau plateau (largeur, hauteur);
 
-   for (int i = 0; i < 100; ++i) {
+   plateau.ajouterRobots(nbr_objets);
+
+   do {
       plateau.afficher();
       plateau.bougerRobots();
-      this_thread::sleep_for(chrono::milliseconds(200));
-   }
+      this_thread::sleep_for(chrono::milliseconds(SLEEP));
+   } while (!plateau.partieFinie());
 
-   return 0;
+   viderBuffer();
+
+   return EXIT_SUCCESS;
 }
